8847b8c9d589bf6413fe026ab4e35d0bec941f8d
Ruben Garcia <garcia.r@opendevpro.com>
2023-06-03 20:47:52 -0300

    setup FormGroup

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+
+  registroForm: FormGroup;
+
+  constructor(private fb:FormBuilder){
+    this.registroForm = this.fb.group({
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required,Validators.email]],
+      password: ['', Validators.required],
+    })
+
+  }
+
+  ngOnInit(): void {
+
+    this.registroForm = this.fb.group({
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required,Validators.email]],
+      password: ['', Validators.required],
+    })
+    
+  }
+
+  crearUsuario(){
+    console.log(this.registroForm)
+    console.log(this.registroForm.valid)
+  }
 
 }
