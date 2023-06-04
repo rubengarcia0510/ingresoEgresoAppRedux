1e88b93110946573e2e1cbe0b93313e02e7ef14b
Ruben Garcia <garcia.r@opendevpro.com>
2023-06-04 18:58:36 -0300

    create crearUsusario method

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +11,8 @@ export class RegisterComponent implements OnInit{
 
   registroForm: FormGroup;
 
-  constructor(private fb:FormBuilder){
+  constructor(private fb:FormBuilder,
+              private authService:AuthService){
     this.registroForm = this.fb.group({
       nombre: ['', Validators.required],
       email: ['', [Validators.required,Validators.email]],
@@ -32,6 +34,16 @@ export class RegisterComponent implements OnInit{
   crearUsuario(){
     console.log(this.registroForm)
     console.log(this.registroForm.valid)
+    if(this.registroForm.invalid) return;
+
+    const {nombre,email,password} = this.registroForm.value;
+    this.authService.crearUsuario(nombre,email,password)
+      .then(credenciales=>{
+        console.log(credenciales)
+      })
+      .catch(err => {
+        console.error(err)
+      })
   }
 
 }
