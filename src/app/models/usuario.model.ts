dc9231a382ed586fe3b847edfef596e808514e5b
Ruben Garcia <garcia.r@opendevpro.com>
2023-06-05 22:21:32 -0300

    add user info to firestore collection

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,13 @@
+export class Usuario {
+
+    public uid:string;
+    public nombre:string;
+    public email:string;
+
+    constructor(uid: string,nombre: string,email: string){
+        this.uid=uid;
+        this.nombre=nombre;
+        this.email=email;
+    }
+    
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, OnDestroy, inject } from '@angular/core';
 import { Auth, User, authState, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@angular/fire/auth';
-import { Subscription, map } from 'rxjs';
+import { CollectionReference, DocumentReference, Firestore, addDoc, collection, collectionData, doc, getFirestore, setDoc,  } from '@angular/fire/firestore';
+import { Observable, Subscription, map } from 'rxjs';
+import { Usuario } from '../models/usuario.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +13,19 @@ export class AuthService implements OnDestroy{
   authState$ = authState(this.auth);
   authStateSubscription: Subscription;
 
+  private firestore: Firestore = inject(Firestore);
+  users$: Observable<Usuario[]> | undefined;
+  usersCollection: CollectionReference=collection(this.firestore, 'users');
+
   constructor() { 
     this.authStateSubscription = this.authState$.subscribe((aUser: User | null) => {
       //handle auth state changes here. Note, that user will be null if there is no currently logged in user.
-   console.log(aUser?.uid);
-   console.log(aUser?.email);
+      console.log(aUser?.uid);
+      console.log(aUser?.email);
+   
   })
+
+  
   }
 
   ngOnDestroy(): void {
@@ -24,10 +33,24 @@ export class AuthService implements OnDestroy{
     
   }
 
+  addUserProfile(uid:string,nombre:string,email:string) {
+    if (!nombre) return;
+
+    let nuevaColl=collection(this.firestore,uid)
+    
+    addDoc(nuevaColl, <Usuario> { uid,nombre,email }).then((documentReference: DocumentReference) => {
+        // the documentReference provides access to the newly created document
+        console.log(documentReference)
+    });
+}
+
   crearUsuario(nombre:string,email:string,password:string){
     console.log({nombre,email,password})
-
-    return createUserWithEmailAndPassword(this.auth,email,password)     
+    
+    return createUserWithEmailAndPassword(this.auth,email,password)
+                .then(user=>{
+                  this.addUserProfile(user.user.uid,nombre,email);
+                })
 
   }
 
