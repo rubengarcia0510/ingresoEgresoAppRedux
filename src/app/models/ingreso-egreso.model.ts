d11f8d94a023e64a15a5f7402e1a02fc52b86041
Ruben Garcia <garcia.r@opendevpro.com>
2023-06-11 13:17:45 -0300

    add ingresoegreso model

---
+++ src/app/models/ingreso-egreso.model.ts
@@ -0,0 +1,13 @@
+export class IngresoEgreso{
+
+    constructor(
+        
+        public tipo:string,
+        public descripcion:string,
+        public monto:number,
+        public uid?:string,
+    ){
+        
+
+    }
+}
