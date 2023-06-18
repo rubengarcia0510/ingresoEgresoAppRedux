dad460d7d7e5b65e0862baa02dcd23c2bb3a7fb9
Ruben Garcia <garcia.r@opendevpro.com>
2023-06-17 23:24:59 -0300

    add store

--- src/app/ingreso-egreso/detalle/detalle.component.ts
+++ src/app/ingreso-egreso/detalle/detalle.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducers';
+import { IngresoEgreso } from 'src/app/models/ingreso-egreso.model';
 
 @Component({
   selector: 'app-detalle',
   templateUrl: './detalle.component.html',
   styleUrls: ['./detalle.component.css']
 })
-export class DetalleComponent {
+export class DetalleComponent implements OnInit{
+borrar(arg0: string|undefined) {
+throw new Error('Method not implemented.');
+}
+
+  ingEgArray:IngresoEgreso[] = []
+
+  constructor(private store:Store<AppState>){}
+
+  ngOnInit(): void {
+    this.store.select('ie')
+        .subscribe(store=>this.ingEgArray=store.items)
+  }
 
 }
