54a542d02045070c1b0917a1c912f4b62f75c9ca
Ruben Garcia <garcia.r@opendevpro.com>
2023-06-17 23:43:10 -0300

    clean ingresos egresos

--- src/app/ingreso-egreso/detalle/detalle.component.ts
+++ src/app/ingreso-egreso/detalle/detalle.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 import { AppState } from 'src/app/app.reducers';
 import { IngresoEgreso } from 'src/app/models/ingreso-egreso.model';
 
@@ -8,16 +9,22 @@ import { IngresoEgreso } from 'src/app/models/ingreso-egreso.model';
   templateUrl: './detalle.component.html',
   styleUrls: ['./detalle.component.css']
 })
-export class DetalleComponent implements OnInit{
+export class DetalleComponent implements OnInit, OnDestroy{
 borrar(arg0: string|undefined) {
 throw new Error('Method not implemented.');
 }
 
   ingEgArray:IngresoEgreso[] = []
+  storeSubs:Subscription = new Subscription;
 
   constructor(private store:Store<AppState>){}
+  ngOnDestroy(): void {
+    this.storeSubs.unsubscribe()
+  }
 
   ngOnInit(): void {
+
+    this.storeSubs = 
     this.store.select('ie')
         .subscribe(store=>this.ingEgArray=store.items)
   }
