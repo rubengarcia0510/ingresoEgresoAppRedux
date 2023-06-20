1c9ba7ba8bdb905f7929d6ec701cf7c549d4179b
Ruben Garcia <garcia.r@opendevpro.com>
2023-06-20 20:15:04 -0300

    ingreso egreso routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,12 +10,12 @@ import { AuthGuard } from './services/auth.guard';
 const routes:Routes = [
   {path:'login',component:LoginComponent},
   {path:'register',component:RegisterComponent},
-  {
-      path:'',
-      component:DashboardComponent,
-      children: dashboardRoutes,
-      canActivate: [AuthGuard]
-    },
+  //{
+    //  path:'',
+      //component:DashboardComponent,
+      //children: dashboardRoutes,
+      //canActivate: [AuthGuard]
+    //},
   {path:'**',redirectTo:''},
 ]
 
