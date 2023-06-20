4d9fed65bae012547ff0c6fa7627819190282124
Ruben Garcia <garcia.r@opendevpro.com>
2023-06-20 15:15:26 -0300

    dinamic username

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -16,6 +16,8 @@ export class DashboardComponent implements OnInit,OnDestroy{
   authSubscribe: Subscription = new Subscription;
   ingresosEgresosSubscription: Subscription = new Subscription;
 
+  usuario:string | undefined;
+
   constructor(private store:Store<AppState>,
               private ingresoEgresoService:IngresoEgresoService){
 
@@ -30,6 +32,8 @@ export class DashboardComponent implements OnInit,OnDestroy{
     
                 .pipe(filter(auth=>auth.user != null))
                 .subscribe( user => {
+                  this.usuario = user.user?.nombre
+
                     this.ingresosEgresosSubscription = this.ingresoEgresoService.ingresoEgresoListener(user.user?.uid)
                         .subscribe(item=>{
                           this.store.dispatch(setItems({items:item}))
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+
+  @Input() usuario:string | undefined
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
